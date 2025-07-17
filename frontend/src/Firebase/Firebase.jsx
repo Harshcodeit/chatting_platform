e41ea79dd330c747d91a435eaf514a5a5aef2645
Harshcodeit <hxsingh25@gmail.com>
2025-07-17 23:24:34 +0530

    added env.example file

--- frontend/src/Firebase/Firebase.jsx
+++ frontend/src/Firebase/Firebase.jsx
@@ -5,27 +5,27 @@ import { getAuth } from "firebase/auth"
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWR1RVa2UnTVSFIXJdHB7esCevFuXj628",
-  authDomain: "chatzone-d3d69.firebaseapp.com",
-  projectId: "chatzone-d3d69",
-  storageBucket: "chatzone-d3d69.firebasestorage.app",
-  messagingSenderId: "160350740414",
-  appId: "1:160350740414:web:7836b971229daf7ea5a3ef",
-  measurementId: "G-1LKCM63KDY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-const app=initializeApp(firebaseConfig)
-const analytics=getAnalytics(app)
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
-const firebaseAuth=getAuth(app)
-const database=getDatabase(app)
+const firebaseAuth = getAuth(app)
+const database = getDatabase(app)
 
-const FirebaseContext=createContext(null)
-export const useFirebase=()=>useContext(FirebaseContext)
+const FirebaseContext = createContext(null)
+export const useFirebase = () => useContext(FirebaseContext)
 
-export const FirebaseContextProvider=(props)=>{
+export const FirebaseContextProvider = (props) => {
     return(
-        <FirebaseContext.Provider>
+        <FirebaseContext.Provider value={{ firebaseAuth, database, analytics }}>
             {props.children}
         </FirebaseContext.Provider>
     )
