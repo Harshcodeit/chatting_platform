486c95f6bca7ddf7a1351dd98ea1342d6ef246f3
Harshcodeit <hxsingh25@gmail.com>
2025-07-23 19:10:43 +0530

    anony-backend

--- firebase_backend/app.py
+++ firebase_backend/app.py
@@ -1,22 +1,31 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from find_match import InterestMatcher  
 
 matcher = InterestMatcher('serviceAccountKey.json')
 
 app = Flask(__name__)
+CORS(app) #Initialized cors for app
 
 @app.route('/process_match', methods=['POST'])
 def process_match():
+    print(f"DEBUG:process_match endpoint hit!")
     data = request.json
+    print(f"DEBUG:Received data :{data}")
     interests = data.get('interests')
+    user_id=data.get('user_id')
+    print(f"DEBUG: Interests extracted: {interests}, User ID extracted: {user_id}")
+    
     if not interests:
+        print(f"DEBUG:Interests were missing!")
         return jsonify({'status': 'error', 'message': 'Interests required'}), 400
     
-    result = matcher.process_matching_request(interests)
+    result = matcher.process_matching_request(interests,user_id=user_id)
     return jsonify(result)
 
 @app.route('/end_match', methods=['POST'])
 def end_match():
+    print(f"DEBUG:end_match endpoint hit!")
     data = request.json
     match_id = data.get('match_id')
     if not match_id:
@@ -27,6 +36,7 @@ def end_match():
 
 @app.route('/waiting_users', methods=['GET'])
 def waiting_users():
+    print("DEBUG:waiting_users endpoint hit!")
     count = matcher.get_waiting_users_count()
     return jsonify({'waiting_users_count': count})
 
